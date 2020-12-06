5b1f60ba328860e700940145806080c06d68afb5
Halid Ali <halid.ali@methodpark.de>
2020-12-06 13:20:38 +0100

    solution of day 6

---
+++ day-06/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalDistinctAnswers = 0;
+            var totalCommonAnswers = 0;
+
+            using (var streamReader = new StreamReader("input-question.txt"))
+            {
+                var distinctAnswers = new HashSet<char>();
+                string commonAnswers = null;
+
+                while (true)
+                {
+                    var answers = streamReader.ReadLine();
+                    if(string.IsNullOrEmpty(answers))
+                    {
+                        totalDistinctAnswers += distinctAnswers.Count;
+                        totalCommonAnswers += commonAnswers.Length;
+                        commonAnswers = null;
+                        distinctAnswers.Clear();
+
+                        if(streamReader.EndOfStream) break;
+                        continue;
+                    }
+
+                    for (int i = 0; i < answers.Length; i++)
+                        distinctAnswers.Add(answers[i]);
+
+                    if(commonAnswers == null)
+                        commonAnswers = answers;
+                    else
+                        commonAnswers = new String(commonAnswers.Intersect<char>(answers).ToArray());
+                }
+            }
+
+            Console.WriteLine($"Total Distinct Answers: {totalDistinctAnswers}");
+            Console.WriteLine($"Total Common Answers: {totalCommonAnswers}");
+        }
+    }
+}
